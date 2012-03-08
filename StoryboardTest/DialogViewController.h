6caf72ed704c0ef2a44b6f8727d55ee01d3671d9
Tsutomu Ogasawara <info@ogaoga.org>
2012-03-08 10:09:03 +0900

    Added a button and a dialog onto the First View.

---
+++ StoryboardTest/DialogViewController.h
@@ -0,0 +1,14 @@
+//
+//  DialogViewController.h
+//  StoryboardTest
+//
+//  Created by 小笠原 努 on 12/03/08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DialogViewController : UIViewController
+- (IBAction)pressCancelButton:(id)sender;
+
+@end
