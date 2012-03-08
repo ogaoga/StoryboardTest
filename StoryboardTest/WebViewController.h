a8a780860853e16cd0cbe8ff755024210dfd10b9
Tsutomu Ogasawara <info@ogaoga.org>
2012-03-08 10:40:07 +0900

    Added sites list and web view.

---
+++ StoryboardTest/WebViewController.h
@@ -0,0 +1,16 @@
+//
+//  WebViewController.h
+//  StoryboardTest
+//
+//  Created by 小笠原 努 on 12/03/08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@property (retain, nonatomic) IBOutlet UIWebView *webView;
+@property (retain, nonatomic) NSURL *url;
+
+@end
